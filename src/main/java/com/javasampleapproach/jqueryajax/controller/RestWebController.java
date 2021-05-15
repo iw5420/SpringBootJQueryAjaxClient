3309b5fa3b94ce953e4afbbb1ad5bbcfce49916b
Ian <iw5420@gmail.com>
2021-05-15 16:01:10 +0800

    1st

--- src/main/java/com/javasampleapproach/jqueryajax/controller/RestWebController.java
+++ src/main/java/com/javasampleapproach/jqueryajax/controller/RestWebController.java
@@ -3,11 +3,7 @@ package com.javasampleapproach.jqueryajax.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.javasampleapproach.jqueryajax.message.Response;
 import com.javasampleapproach.jqueryajax.model.Customer;
@@ -29,7 +25,15 @@ public class RestWebController {
 		cust.add(customer);
 		
 		// Create Response Object
-		Response response = new Response("Done", cust);
+		Response response = new Response("Done", customer);
 		return response;
 	}
+
+	@PostMapping(value = "/yourUrl")
+	public Response yourUrl(@RequestParam String data) {
+		System.out.println(data);
+		return new Response("Done", data);
+	}
+
+
 }
--- src/main/resources/static/js/postrequest.js
+++ src/main/resources/static/js/postrequest.js
@@ -4,23 +4,21 @@ $( document ).ready(function() {
     $("#customerForm").submit(function(event) {
 		// Prevent the form from submitting via the browser.
 		event.preventDefault();
-		ajaxPost();
+		ajaxUrlPost();
 	});
     
     
     function ajaxPost(){
-    	
     	// PREPARE FORM DATA
     	var formData = {
     		firstname : $("#firstname").val(),
     		lastname :  $("#lastname").val()
     	}
-    	
     	// DO POST
     	$.ajax({
 			type : "POST",
 			contentType : "application/json",
-			url : window.location + "/api/customer/save",
+			url : window.location + "api/customer/save",
 			data : JSON.stringify(formData),
 			dataType : 'json',
 			success : function(result) {
@@ -36,12 +34,40 @@ $( document ).ready(function() {
 				console.log("ERROR: ", e);
 			}
 		});
-    	
     	// Reset FormData after Posting
     	resetData();
-
     }
-    
+
+	function ajaxUrlPost(){
+		// PREPARE FORM DATA
+		var formData = {
+			firstname : $("#firstname").val(),
+			lastname :  $("#lastname").val()
+		}
+		// DO POST
+		$.ajax({
+			type : "POST",
+			contentType : "application/json",
+			url : window.location + "api/customer/yourUrl?data="+$("#firstname").val(),
+			//data : JSON.stringify(formData),
+			dataType : 'json',
+			success : function(result) {
+				if(result.status == "Done"){
+					$("#postResultDiv").html("<strong>success</strong>");
+				}else{
+					$("#postResultDiv").html("<strong>Error</strong>");
+				}
+				console.log(result);
+			},
+			error : function(e) {
+				alert("Error!")
+				console.log("ERROR: ", e);
+			}
+		});
+		// Reset FormData after Posting
+		resetData();
+	}
+
     function resetData(){
     	$("#firstname").val("");
     	$("#lastname").val("");
